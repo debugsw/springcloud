3318b50d46e95d0f3ea59bc15fc2156c6971ae8c
liushuai <shuai012192@126.com>
2020-05-11 13:48:01 +0800

    base

--- src/main/java/com/example/cloud/demo/DemoApplication.java
+++ src/main/java/com/example/cloud/demo/DemoApplication.java
@@ -2,11 +2,22 @@ package com.example.cloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @Author: ls
+ * @Description:
+ * @Date: 2019/1/12 18:45
+ **/
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
